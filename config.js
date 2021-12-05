019152bb81834f843b3f77e30652c92ddeb5baf3
Deris pewsss <94625813+derispewss@users.noreply.github.com>
2021-12-06 04:58:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'derisapi',
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais'
